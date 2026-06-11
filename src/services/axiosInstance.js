88deb8365b79613222abdbc672f9b3f259d6708a
Yash Gupta <yashgupta5094@gmail.com>
2026-06-11 22:36:58 +0530

    Update axios baseURL to include API version

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://yt-clone-backend-mpjm.onrender.com',
+    baseURL: 'https://yt-clone-backend-mpjm.onrender.com/api/v1',
     withCredentials: true,
 });
 
